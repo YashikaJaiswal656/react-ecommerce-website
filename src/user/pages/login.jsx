8440c764e725561f491dfa74c30ffb9cff431c68
Yashika Jaiswal <jaiswalyashika79@gmail.com>
2025-05-02 11:30:01 +0530

    Update login.jsx
    changes in path

--- src/user/pages/login.jsx
+++ src/user/pages/login.jsx
@@ -21,7 +21,7 @@ const LoginPage = () => {
 
 
     try {
-      const response = await fetch("http://localhost:3000/login", {
+      const response = await fetch("/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
